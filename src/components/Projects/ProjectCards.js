c796263630dab25fac482d4d9b012562442abb28
itsDenMat <dennismateo93@gmail.com>
2022-11-05 13:15:31 -0700

    Initialize ProjectCards.js

---
+++ src/components/Projects/ProjectCards.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
+import { BilinkExternal } from 'react-icons/bi';
+
+function ProjectCards(props) {
+    return (
+
+    )
+}
