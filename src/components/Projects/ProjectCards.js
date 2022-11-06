14bb3051c0e62cc84714a5a93ea4cc64ae7132ed
itsDenMat <dennismateo93@gmail.com>
2022-11-05 21:09:54 -0700

    Finalize format of ProjectCards

--- src/components/Projects/ProjectCards.js
+++ src/components/Projects/ProjectCards.js
@@ -5,6 +5,16 @@ import { BilinkExternal } from 'react-icons/bi';
 
 function ProjectCards(props) {
     return (
-
-    )
+        <Card className='project-card-view'>
+            {/* INSERT IMAGE HERE */}
+            <Card.Body>
+                {/* INSERT TITLE */}
+                {/* INSERT TECHNOLOGY USED */}
+                {/* INSERT DESCRIPTION */}
+                {/* INSERT BUTTON TO VIEW LIVE PAGE OF PROJECT */}
+            </Card.Body>
+        </Card>
+    );
 }
+
+export default ProjectCards;
